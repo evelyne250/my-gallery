655e65229bc8758a543771949f8e28be59754973
pascaline <passyish5@gmail.com>
2019-10-09 17:09:15 +0200

    added models to the admin page

--- photos/tests.py
+++ photos/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Category,Location,Image
+from django.contrib import auth
 # Create your tests here.
 class CategoryTestClass(TestCase):
 
@@ -16,3 +17,14 @@ class CategoryTestClass(TestCase):
         self.travel.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user('test@dom.com', 'test@dom.com', 'pass')
+        self.u.is_staff = True
+        self.u.is_superuser = True
+        self.u.is_active = True
+        self.u.save()
+
+    def testLogin(self):
+        self.client.login(username='test@dom.com', password='pass')
