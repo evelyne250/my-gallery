c07033115616af6399b0061337de55d4d5eed779
pascaline <passyish5@gmail.com>
2019-10-09 12:15:29 +0200

    added dynamic content into views to display particular days

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+import datetime as dt
 from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to my Gallery')
+
+def pictures(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
